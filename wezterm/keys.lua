df55af5d559c2ec38343749502efe19701481b2a
Aman <aman.kumar@softsensor.ai>
2023-04-11 17:39:21 +0530

    Added keys.lua

---
+++ wezterm/keys.lua
@@ -0,0 +1,36 @@
+local wezterm  = require 'wezterm'
+local act = wezterm.action
+
+return {
+   {
+      key = 'c',
+      mods = 'SHIFT|CTRL',
+      action = act.CopyTo 'ClipboardAndPrimarySelection',
+   },
+   {
+      key = 'v',
+      mods = 'SHIFT|CTRL',
+      action = act.PasteFrom 'Clipboard'
+   },
+   {
+      key = '=',
+      mods = 'CTRL',
+      action = act.IncreaseFontSize,
+   },
+   {
+      key = '-',
+      mods = 'CTRL',
+      action = act.DecreaseFontSize,
+   },
+   {
+      key = 'X',
+      mods = 'SHIFT|CTRL',
+      action = act.ActivateCopyMode
+   },
+   {
+    key = 'F',
+    mods = 'SHIFT|CTRL',
+    action = act.Search { CaseInSensitiveString = '' },
+  },
+
+}
